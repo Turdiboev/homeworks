04f149710e91ed6baa4ec7b7dbcbfe181cf096f7
Turdiboev <turdiboevshukurullo1@gmail.com>
2026-02-07 16:58:15 +0500

    Add files via upload

---
+++ lesson1 (2).py
@@ -0,0 +1,20 @@
+# Answers. Shukurullo Turdiboev
+
+# 1.
+# Analyzing big data makes decision-making easier. Data analysis is essential for gaining deeper insights into products and customers. Additionally, through this analysis, it becomes easier to track how well each product is selling.
+
+
+# 2.
+# Python is considered faster and more powerful than Excel when working with large datasets. Through its libraries, Python can easily analyze large amounts of data, while Excel does not have such capabilities
+
+# 3.
+# I would analyze the data automatically. Using Python, it is possible to quickly organize, calculate, and extract important results from the data. This is much faster and more convenient than doing it manually.
+
+# 4.
+# Python is useful in BI processes for data cleaning, organizing, analysis, and creating visualizations. It helps work with large amounts of data quickly and efficiently.
+
+# 5.
+# Using Python, profit data for each year can be collected and compared. The results can be shown in a table or a simple chart to see which years had higher or lower profits.
+
+# 6.
+# If you don’t know Python, it is hard to analyze large datasets quickly and efficiently. You would spend a lot of time manually sorting, calculating, and creating charts, and you are more prone to errors.
